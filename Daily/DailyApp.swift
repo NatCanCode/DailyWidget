7bb7b2357e0bf74ed4bf662439a9e3b8d397e130
NatCanCode <nathalie.piat1@gmail.com>
2023-11-22 14:12:55 +0100

    Initial Commit

---
+++ Daily/DailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyApp.swift
+//  Daily
+//
+//  Created by N N on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct DailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
